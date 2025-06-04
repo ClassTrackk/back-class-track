c780d34a052ad9bf4a5e3203ec3b6403223be5ba
Michele Boninelli <m.boni2000@gmail.com>
2025-06-04 19:23:09 +0200

    update utenti

--- back-class-track/Models/Entities/Utente.cs
+++ back-class-track/Models/Entities/Utente.cs
@@ -3,20 +3,20 @@
     public class Utente
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
-        public string? Cognome { get; set; }
-
-        public string? Email { get; set; }
-        public string? PasswordHash { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
         public Mansione Ruolo { get; set; }
     }
 
-        public enum Mansione
-        {
-            None = 0,
-            Studente,
-            Docente,
-            Tutor,
-            Amministratore
-        }
+    public enum Mansione
+    {
+        None = 0,
+        Studente,
+        Docente,
+        Tutor,
+        Amministratore
+    }
+
 }
