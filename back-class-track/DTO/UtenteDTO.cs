3c80a63742c31fca64a9fe2c55488271dd286615
Michele Boninelli <m.boni2000@gmail.com>
2025-06-05 17:23:31 +0200

    Creato primo DTO

---
+++ back-class-track/DTO/UtenteDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace back_class_track.DTO
+{
+    public class UtenteDTO
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string email { get; set; }
+
+    }
+}
