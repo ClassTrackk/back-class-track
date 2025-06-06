2e7e9ee8415db84106c055e22aebe869c9ff8fe0
Michele Boninelli <m.boni2000@gmail.com>
2025-06-06 15:50:37 +0200

    Inizio controllers per amministratore

---
+++ back-class-track/Controllers/GestioneUtenti/UtenteController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using back_class_track.Data;
+using back_class_track.Models.Entities;
+using back_class_track.DTO.Utenti;
+
+
+namespace back_class_track.Controllers.GestioneUtenti
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UtentiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UtentiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        #region CONTROLLERS PER AMMINISTRATORE
+        //ROUTE PER AVERE TUTTI GLI UTENTI NEL DB
+        // GET: api/users
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<Utente>>> GetUtenti()
+        {
+            
+            var utenti = await _context.Utenti
+                .Select(u => new UserDTO
+            {
+                id = u.id,
+                nome = u.nome,
+                cognome = u.cognome,
+                email = u.email,
+                ruolo = u.ruolo,
+            }).ToListAsync();
+
+            return Ok(utenti);
+        }
+
+        //ROUTE PER AVERE UN SINGOLO UTENTE
+        //GET: api/users/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUtente(int id)
+        {
+            var utente = await _context.Utenti
+                .Where(u => u.id == id)
+                .Select(u => new UserDTO
+                {
+                    id = u.id,
+                    nome = u.nome,
+                    cognome = u.cognome,
+                    email = u.email,
+                    ruolo = u.ruolo,
+                })
+                .FirstOrDefaultAsync();
+
+            if (utente == null)
+                return NotFound(new { message = "Utente non trovato" });
+
+            return Ok(utente);
+        }
+    }
+
+    #endregion
+}
+
