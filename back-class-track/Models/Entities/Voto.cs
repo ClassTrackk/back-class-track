d5ad8fd8015b9fc8fa6d641dd226da4e3a2a41a9
Michele Boninelli <m.boni2000@gmail.com>
2025-06-10 11:58:52 +0200

    Add Voto models
    Aggiunto anche la tabella nel db

---
+++ back-class-track/Models/Entities/Voto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace back_class_track.Models.Entities
+{
+    public class Voto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        public float valutazione { get; set; }
+
+        [Required]
+        public DateTime dataVerifica { get; set; }
+        
+        public string descrizione { get; set; }
+
+        [ForeignKey("Studente")]
+        public int studenteId { get; set; }
+        public Utente studente { get; set; }
+
+        [ForeignKey("Corso")]
+        public int corsoId { get; set; }
+        public Corso corso { get; set; }
+
+    }
+}
